82834209da6d0358c1f34c07487ac55df3949e1c
OMAR ALSHEHRI <166976917+Moghram07@users.noreply.github.com>
2024-08-21 17:17:13 +0300

    Add files via upload

---
+++ src/main/java/com/example/schoolmangexcersize/DTO/AddressDTO.java
@@ -0,0 +1,24 @@
+package com.example.schoolmangexcersize.DTO;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AddressDTO {
+
+    private Integer teacher_id;
+
+    @NotEmpty
+    private String state;
+
+    @NotEmpty
+    private String city;
+
+    @NotEmpty
+    private String street;
+
+    @NotEmpty
+    private String buildingNumber;
+}
---
+++ src/main/java/com/example/schoolmangexcersize/Repository/TeacherRepository.java
@@ -0,0 +1,9 @@
+package com.example.schoolmangexcersize.Repository;
+
+import com.example.schoolmangexcersize.Model.Teacher;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeacherRepository extends JpaRepository<Teacher, Integer> {
+
+    Teacher findTeacherById(Integer id);
+}
